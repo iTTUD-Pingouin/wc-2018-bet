c5cabfb9bae0dde6327cacdd79e0dab48d3eb338
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-19 18:27:34 +0200

    users controller created. show, edit, update and destroy features implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UsersController < ApplicationController
+before_action :find_user_by_id
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:pseudo)
+  end
+
+  def find_user_by_id
+    @user = User.find(params[:id])
+  end
 end
