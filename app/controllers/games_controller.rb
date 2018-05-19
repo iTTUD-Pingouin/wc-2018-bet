70bb88b2ec832972437cc64e1382962d9f47bceb
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-19 17:12:23 +0200

    all features of games controller implemented

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,47 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.all.sort
   end
 
   def show
-
+    @game = Game.find(params[:id])
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to game_path(@game)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:team1, :team2, :score1, :score2)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
   has_many :bets
   has_many :users, through: :bets
+
+  validates :team1, presence: true
+  validates :team2, presence: true
 end
