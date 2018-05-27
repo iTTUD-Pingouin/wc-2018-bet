18d9ef64a246c68a02fc21a2fee0e9c009286fe9
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-27 19:17:46 +0200

    modified navbar; removing search bar and finding a way to make the selector

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,14 +5,6 @@ class GamesController < ApplicationController
   def index
     @poules = Poule.all
     @games = policy_scope(Game)
-    @games_A = Game.select { |game| game.poule == "A"}
-    @games_B = Game.select { |game| game.poule == "B"}
-    @games_C = Game.select { |game| game.poule == "C"}
-    @games_D = Game.select { |game| game.poule == "D"}
-    @games_E = Game.select { |game| game.poule == "E"}
-    @games_F = Game.select { |game| game.poule == "F"}
-    @games_G = Game.select { |game| game.poule == "G"}
-    @games_H = Game.select { |game| game.poule == "H"}
   end
 
   def show
