0177dcfdb33ac02cc85d68c438a059453b2391bb
Guillaume Dutti <guillaume.dutti@live.fr>
2018-06-03 20:56:59 +0200

    add figaro gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_27_140913) do
+ActiveRecord::Schema.define(version: 2018_06_03_185405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_05_27_140913) do
     t.string "pseudo"
     t.boolean "admin", default: false
     t.bigint "group_id", default: 1
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["group_id"], name: "index_users_on_group_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
